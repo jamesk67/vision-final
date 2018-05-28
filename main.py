6c351b89c68d328ed28a87c99077dd498884b9c8
jamesk67 <jamesgki.kim67@gmail.com>
2018-05-28 14:49:54 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,116 @@
+from utils import argParser
+from dataloader import CifarLoader
+import matplotlib.pyplot as plt
+import numpy as np
+import models
+import torch
+import pdb
+
+
+def train(net, dataloader, optimizer, criterion, epoch, device):
+
+    running_loss = 0.0
+    total_loss = 0.0
+
+    for i, data in enumerate(dataloader.trainloader, 0):
+        # get the inputs
+        inputs, labels = data
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+
+        # regression tensor for MSELoss, comment out the following four lines
+        # to use CrossEntropyLoss
+        #rlabels = labels.new_zeros((labels.size()[0], 10), dtype=torch.float)
+        #for i in range(labels.size()[0]):
+         #   rlabels[i, labels[i].item()] = 1
+        #labels = rlabels
+
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        total_loss += loss.item()
+        if (i + 1) % 2000 == 0:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+    print('Final Summary:   loss: %.3f' %
+          (total_loss / i))
+
+
+def test(net, dataloader, device, tag=''):
+    correct = 0
+    total = 0
+    if tag == 'Train':
+        dataTestLoader = dataloader.trainloader
+    else:
+        dataTestLoader = dataloader.testloader
+    with torch.no_grad():
+        for data in dataTestLoader:
+            images, labels = data
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('%s Accuracy of the network: %d %%' % (tag,
+        100 * correct / total))
+
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
+    with torch.no_grad():
+        for data in dataTestLoader:
+            images, labels = data
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = net(images)
+            _, predicted = torch.max(outputs, 1)
+            c = (predicted == labels).squeeze()
+            for i in range(len(labels)):
+                label = labels[i]
+                class_correct[label] += c[i].item()
+                class_total[label] += 1
+
+
+    for i in range(10):
+      print('%s Accuracy of %5s : %2d %%' % (
+       tag, dataloader.classes[i], 100 * class_correct[i] / class_total[i]))
+
+def main():
+
+    args = argParser()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
+    cifarLoader = CifarLoader(args)
+    net = args.model()
+    net = net.to(device)
+    #print('The log is recorded in ')
+    #print(net.logFile.name)
+
+    criterion = net.criterion().to(device)
+    optimizer = net.optimizer()
+
+    for epoch in range(args.epochs):  # loop over the dataset multiple times
+        net.adjust_learning_rate(optimizer, epoch, args)
+        train(net, cifarLoader, optimizer, criterion, epoch, device)
+        if epoch % 1 == 0: # Comment out this part if you want a faster training
+            test(net, cifarLoader, device, 'Train')
+            test(net, cifarLoader, device, 'Test')
+
+    print("done testing")
+    #print('The log is recorded in ')
+    #print(net.logFile.name)
+
+if __name__ == '__main__':
+    main()
---
+++ models.py
@@ -0,0 +1,96 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import os
+import datetime
+import pdb
+import time
+import torchvision.models as torchmodels
+
+class BaseModel(nn.Module):
+    def __init__(self):
+        super(BaseModel, self).__init__()
+        #if not os.path.exists('logs'):
+         #   os.makedirs('logs')
+        ts = time.time()
+        #st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S_log.txt')
+        #st = 'logs/' + st
+        #self.logFile = open(st, 'w+')
+
+    def log(self, str):
+        print(str)
+        self.logFile.write(str + '\n')
+
+    def criterion(self):
+        return nn.CrossEntropyLoss()
+        #return nn.MSELoss()
+
+    def optimizer(self):
+        return optim.SGD(self.parameters(), lr=0.001)
+
+    def adjust_learning_rate(self, optimizer, epoch, args):
+        lr = args.lr  # TODO: Implement decreasing learning rate's rules
+        #if (epoch + 1) % 50 == 0:
+         #   lr = lr * 0.1
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+
+    # never used this to make training run faster
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+       
+
+
+class LazyNet(BaseModel):
+    def __init__(self):
+        super(LazyNet, self).__init__()
+        # TODO: Define model here
+        self.fc1 = nn.Linear(32 * 32 * 3, 10);
+
+    def forward(self, x):
+        # TODO: Implement forward pass for LazyNet
+        x = x.view(-1, 32 * 32 * 3);
+        x = self.fc1(x);
+        return x
+
+class BoringNet(BaseModel):
+    def __init__(self):
+        super(BoringNet, self).__init__()
+        # TODO: Define model here
+        self.fc1 = nn.Linear(32 * 32 * 3, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        # TODO: Implement forward pass for BoringNet
+        x = x.view(-1, 32 * 32 * 3)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+class CoolNet(BaseModel):
+    def __init__(self):
+        super(CoolNet, self).__init__()
+        # TODO: Define model here
+        self.conv1 = nn.Conv2d(3, 27, 3)
+        self.conv2 = nn.Conv2d(27, 16, 5)
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        # TODO: Implement forward pass for CoolNet
+        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
